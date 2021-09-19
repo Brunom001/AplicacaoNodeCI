98eec4f1a80716a3669adea9c935ea4ac70d9619
Bruno Morais <bmo.morais@hotmail.com>
2021-09-18 22:29:28 -0300

    Teste de operações calculadora

---
+++ calculadora.test.js
@@ -0,0 +1,16 @@
+//import {expect, test} from '@jest/globals';
+//import * as calculadora from './calculadora.js';
+const calculadora = require('./calculadora');
+
+test("Testando  soma de 1 + 1", () => {
+    expect(calculadora.soma(1,1)).toBe(2);
+})
+
+test("Testando  subtração de 2 - 1", () => {
+    expect(calculadora.subtracao(2,1)).toBe(1);
+})
+
+test("Testando  divisao de 42 / 6", () => {
+    expect(calculadora.divisao(42,6)).toBe(9);
+})
+
